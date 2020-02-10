86afd23b73a7cf84913af74997b682c90e1f8793
atloz <rekenei.zolta@gmail.com>
2020-02-10 09:48:55 +0100

    stabil alap

--- src/hu/petrik/Main.java
+++ src/hu/petrik/Main.java
@@ -11,11 +11,6 @@ public class Main {
 
         SakkTabla t = new SakkTabla();
         System.out.println(t);
-        t.lep(6,1,4,1); //világos gyalog kilép
-        t.lep(1,1,3,1);
-        t.lep(6,2,4,2);
-        t.lep(3,1,4,2); //sötét gyalog leüt
-        System.out.println(t);
 
     }
 }
